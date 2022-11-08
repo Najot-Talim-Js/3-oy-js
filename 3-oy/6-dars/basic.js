16f7e97727a0164c0928a58b1ecff1e21b3f2b89
Umar Djuraev <umar.djuraev@MacBook-Air.local>
2022-11-08 09:13:12 +0500

    change: files

---
+++ 3-oy/6-dars/basic.js
@@ -0,0 +1,128 @@
+
+// var person = {
+//     // key        value
+//     lastName: 'Akmalov',
+//     firstName: 'Temur',
+//     age: 22,
+//     married: true,
+//     car: null,
+//     address: {
+//         zipCode: 100110,
+//         country: 'Uzbekiston',
+//         state: 'Toshkent',
+//         province: 'Andijon',
+//         disrict: 'Baliqchi'
+//     }
+// }
+
+
+
+
+// malumot olish
+// var info = `Familiyasi: ${person['lastName']} , Ismi: ${person.firstName}, Yoshi: ${person.age} da`
+// var getAge = 'age'
+
+// console.log(person.age);
+
+// console.log(person[getAge]);
+
+
+
+// console.log(person2[getAge]);
+
+
+// malumot qoyish
+
+// var person2 = {
+//     age: 66
+// }
+
+// console.log(person2);
+
+// person2.name = 'umar'
+// person2['married'] = true 
+// person2.address = {
+//     title: 'asdasdsa'
+// }
+
+
+// console.log(person2);
+
+// malumotni ochirish
+
+
+// delete person2.name
+// delete person2.age
+// delete person2.address
+
+// console.log(person2);
+
+
+
+
+
+// var fruits = ['olma', 'shoftoli', 'anor', 'uzum', 'nok', 'xurmo', 'behi']
+
+//malumot olish
+// console.log(fruits['2']);
+// console.log(fruits[2]);
+
+//malumot qotish
+
+// fruits[7]= 'ooo'
+// fruits[12]= 'aaa'
+// fruits[0] = 'asdjkhasjkdhjaskd'
+
+// malumot ichirish
+
+// delete fruits[0]
+
+// console.log(fruits);
+
+
+
+// ~~~~~~~~~~
+
+
+
+// darajaga oshireadi
+console.log(Math.pow(2, 4));
+
+
+// eng kotta qiymatni aniqlaydi
+// console.log(Math.max(2,4,10,288));
+
+
+// eng kichik qiymatni aniqlaydi
+// console.log(Math.min(2,4,10,1));
+
+// pastga 
+// console.log(Math.floor(2.920));
+
+
+// teppaga 
+// console.log(Math.ceil(2.001));
+
+
+
+// qaysi tamonga tortsa 
+// console.log(Math.round(2.7));
+
+//random son chiqaradi 
+// console.log(Math.random());
+
+
+// console.log(Math.floor(Math.random() * 10));
+
+
+
+// ildizini chiqaradi
+// console.log(Math.sqrt(5));
+
+
+// kob ildiz chiqaradi
+// console.log(Math.cbrt(8));
+
+
+
+
